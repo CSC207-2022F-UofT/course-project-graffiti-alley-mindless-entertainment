c00af539af7a453964e4ef848e523c74eb5358c4
bw55555 <brianwang2013@gmail.com>
2022-11-11 10:01:47 -0500

    add some docstrings

--- src/main/java/interfaces/InputValidator.java
+++ src/main/java/interfaces/InputValidator.java
@@ -1,4 +1,9 @@
 package interfaces;
 
 public interface InputValidator {
+    /**
+     * @param input the user input to validate
+     * @return whether the given input is valid
+     */
+    public boolean validateInput(String input);
 }
