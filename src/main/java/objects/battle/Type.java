914ac81f686abd3728a5bdc03a0ba4b0bf539731
Charlie Tao <charlietaoxl@gmail.com>
2022-11-16 16:56:23 -0500

    Added docstring to Type enum

--- src/main/java/objects/battle/Type.java
+++ src/main/java/objects/battle/Type.java
@@ -1,6 +1,9 @@
 package objects.battle;
 
 public enum Type {
+    /** Enum that holds constants FIRE, WATER, GRASS for type advantages
+     *  when doing battle calculations. FIRE < WATER < GRASS < FIRE.
+     */
     FIRE,
     WATER,
     GRASS
