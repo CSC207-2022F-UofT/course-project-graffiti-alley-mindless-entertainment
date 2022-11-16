12239921692c059165dc46dde54bf2c0b6071556
bw55555 <brianwang2013@gmail.com>
2022-11-15 20:52:50 -0500

    initial commit of PauseMenuManager

--- src/main/java/menus/PauseMenuManager.java
+++ src/main/java/menus/PauseMenuManager.java
@@ -1,5 +1,66 @@
 package menus;
 
-public class PauseMenuManager {
+import interfaces.State;
+import managers.StateManager;
+import menus.options.ChangeOptionsState;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+
+public class PauseMenuManager extends StateManager {
+
+    private List<String> pauseMenuOptions;
+    private String optionsCommand;
+
+    private String saveCommand;
+    private String exitCommand;
+
+    PauseMenuChoiceStateFactory pauseMenuChoiceStateFactory;
+
+    MenuType currMenuType;
+
+    public PauseMenuManager(PauseMenuChoiceStateFactory pauseMenuChoiceStateFactory) {
+        this.pauseMenuChoiceStateFactory = pauseMenuChoiceStateFactory;
+        this.optionsCommand = "options";
+        this.saveCommand = "save";
+        this.exitCommand = "exit";
+        this.pauseMenuOptions = Arrays.asList(optionsCommand, saveCommand, exitCommand);
+
+        currState = pauseMenuChoiceStateFactory.createPauseMenuChoiceState(pauseMenuOptions);
+    }
+
+    /**
+     * @param input from the user
+     * @return the next state
+     */
+    @Override
+    public State nextState(String input) {
+
+        if (currMenuType == MenuType.PAUSE) {
+            if (Objects.equals(input, optionsCommand)) {
+                return new ChangeOptionsState();
+            } else if (Objects.equals(input, saveCommand)) {
+                //to be implemented later
+                return null;
+            } else {
+                return null;
+            }
+        } else {
+            if (Objects.equals(input, exitCommand)) {
+                return pauseMenuChoiceStateFactory.createPauseMenuChoiceState(pauseMenuOptions);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void initialize() {
+        currState = pauseMenuChoiceStateFactory.createPauseMenuChoiceState(pauseMenuOptions);
+    }
 }
