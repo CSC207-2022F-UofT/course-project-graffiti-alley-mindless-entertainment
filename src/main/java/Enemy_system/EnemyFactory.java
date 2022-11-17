0c3ae1d155d65a092a74794f1855013b4c64b147
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-16 21:40:42 -0500

    added createBoss from database

--- src/main/java/Enemy_system/EnemyFactory.java
+++ src/main/java/Enemy_system/EnemyFactory.java
@@ -16,11 +16,12 @@ public class EnemyFactory {
     public static Enemy getEnemy(String name){
         EnemyData enemyData = databaseManager.fetchEnemyData(name);
         ArrayList<Skill> skills = new ArrayList<Skill>();
-        for (i in enemyData.skills) {
+        for (String i:enemyData.skills) {
             SkillData skillData = databaseManager.fetchSkillData(i);
             int damage = skillData.damage;
             String type = skillData.type;
-            Skill skill = new Skill(i, damage, type);
+            int lag = skillData.lag;
+            Skill skill = new Skill(i, damage, lag, type);
             skills.add(skill);
         }
         EnemyAI enemyType;
@@ -31,6 +32,38 @@ public class EnemyFactory {
         }
         Enemy enemy = new Enemy(name, 1, skills, enemyType);
         return enemy;
+    }
+
+    public static Boss getBoss(String name){
+        BossData bossData = databaseManager.fetchBossData(name);
+        ArrayList<Skill> skills = new ArrayList<Skill>();
+        for (String i:bossData.skills) {
+            SkillData skillData = databaseManager.fetchSkillData(i);
+            int damage = skillData.damage;
+            String type = skillData.type;
+            int lag = skillData.lag;
+            Skill skill = new Skill(i, damage, lag, type);
+            skills.add(skill);
+        }
+        EnemyAI bossType;
+        if (bossData.type == "Passive") {
+            bossType = Enemy_system.PassiveAI;
+        } else{
+            bossType = Enemy_system.AggresiveAI;
+        }
+
+        if(bossData.gimmick == "Health"){
+            Gimmick gimmick = new Enemy_system.HealthGimmick();
+        } if(bossData.gimmick == "Attack"){
+            Gimmick gimmick = new Enemy_system.AttackGimmick();
+        } if(bossData.gimmick == "Damage"){
+            Gimmick gimmick = new Enemy_system.DamageGimmick();
+        } if(bossData.gimmick == "Turn"){
+            Gimmick gimmick = new Enemy_system.TurnGimmick();
+
+
+        Boss boss = new Boss(name, 1, skills, enemyType, gimmick);
+        return boss;
     }
 
     public static Boss createBoss4() {
