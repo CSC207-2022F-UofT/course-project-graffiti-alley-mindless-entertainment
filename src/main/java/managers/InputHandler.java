302fdb7341704fd9af3f661804e111fe83b2a712
zoe <zoe.he@mail.utoronto.ca>
2022-11-17 01:49:13 -0500

    fixed InputHandler

--- src/main/java/managers/InputHandler.java
+++ src/main/java/managers/InputHandler.java
@@ -1,6 +1,5 @@
 package managers;
 
-import com.sun.corba.se.spi.orbutil.fsm.Input;
 import interfaces.InputValidator;
 
 import java.util.Scanner;
@@ -8,8 +7,8 @@ import java.util.Scanner;
 public class InputHandler{
 
     String getChoice(InputValidator validator) {
+        Scanner input = new Scanner(System.in);
         while (true) {
-            Scanner input = new Scanner(System.in);
             String choice = input.next();
             if (validator.validateInput(choice)) {
                 return choice;
