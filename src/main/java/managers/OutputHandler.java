bba011b646e2ea14193714d68c0cdb4556da3573
zoe <zoe.he@mail.utoronto.ca>
2022-11-16 23:09:33 -0500

    InputHandler and OutputHandler done

--- src/main/java/managers/OutputHandler.java
+++ src/main/java/managers/OutputHandler.java
@@ -1,11 +1,13 @@
 package managers;
 
+import java.util.List;
+
 public class OutputHandler {
     public void generateText(String text) {
         System.out.println(text);
     }
 
-    public void generateTextWithOptions(String text, String[] options) {
+    public void generateTextWithOptions(String text, List<String> options) {
         System.out.println(text);
         String opt="a";
         for (String option: options) {
