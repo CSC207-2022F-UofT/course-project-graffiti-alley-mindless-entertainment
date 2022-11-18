88c7ba11679fab82f78a723e9c31dc659faeabaf
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-17 20:26:50 -0500

    default AI with chance of attack

---
+++ src/main/java/Battle/Enemy/DefaultAI.java
@@ -0,0 +1,29 @@
+package Battle.Enemy;
+
+import java.util.Objects;
+import java.util.Random;
+
+public class DefaultAI implements EnemyAI{
+
+    public int attackChance;
+
+    public DefaultAI(int n){
+        this.attackChance = n;
+    }
+
+    @Override
+    public String respond(String input) {
+        if (Objects.equals(input, "use skill")) {
+            Random rand = new Random();
+            int upperbound = 101;
+            int int_random = rand.nextInt(upperbound);
+            if(int_random > this.attackChance){
+                return "use skill";
+            } else{
+                return "use potion";
+            }
+        } else{
+            return "use skill";
+        }
+    }
+}
