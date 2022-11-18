ccc8e224afbbff306ac8e282e1e02e76cc95d328
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-18 10:38:37 -0500

    Revert "I have added an enemy AI class that responds to what the user does. There are two types of enemy AIs which are aggressive AI and Passive AI. Aggressive AI is more aggressive and attacks more often. On the other hand, the passive AI plays safer and heals more often."
    This reverts commit dad37a820885f6fa94b3918777c3d9be90c5f5af.

--- src/main/java/Battle/Enemy/EnemyAI.java
+++
@@ -1,9 +0,0 @@
-package Battle.Enemy;
-
-public interface EnemyAI {
-    /**
-     * @param input: input from the user in string
-     * @return enemy's response to the input in string
-     */
-    public String respond(String input);
-}
