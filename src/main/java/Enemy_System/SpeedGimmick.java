e32cc02055138f750955df44a64356cdf34e664f
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-18 09:30:38 -0500

    fixed to speed gimmicka dn type gimmick so it does not deoend in player and also fixed factory

---
+++ src/main/java/Enemy_System/SpeedGimmick.java
@@ -0,0 +1,25 @@
+package Enemy_system;
+package objects.charactoers.player;
+public class SpeedGimmick implements Gimmick{
+    private EnemyInfo enemyInfo;
+
+    public SpeedGimmick(EnemyInfo enemyInfo){
+        this.enemyInfo = enemyInfo;
+    }
+
+    public void useGimmick(){ // return true if the gimmick has been used by the boss. This method
+        // returns boolean because if the gimmick is used, maybe some dialouges will be called in battle state class.
+        //(whenever this method return true)
+        int newSpeed = Math.ceil(this.enemyInfo.getSpeed*0.5);
+        this.enemyInfo.changeSpeed(newSpeed);
+    }
+
+    public boolean checkGimmick(){
+        if(this.enemyInfo.getHealth() < 30){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
---
+++ src/main/java/Enemy_System/TypeGimmick.java
@@ -0,0 +1,41 @@
+package Enemy_system;
+
+package Enemy_System;
+
+import objects.character.Boss;
+import Enemy_Entities.Gimmick;
+
+public class TypeGimmick implements Enemy_system.Gimmick {
+    private EnemyInfo enemyInfo;
+    //In this turn gimmick, whenever, the gimmick is triggered, only enemy
+    // will attack in the next turn.
+
+    public TypeGimmick(EnemyInfo enemyInfo){
+        this.enemyInfo = enemyInfo;
+    }
+
+    public void useGimmick(){ // return true if the gimmick has been used by the boss. This method
+        // returns boolean because if the gimmick is used, maybe some dialouges will be called in battle state class.
+        //(whenever this method return true)
+
+        Type type = this.enemyInfo.getType();
+        if(type == Type.WATER){
+            this.enemyInfo.setType(Type.FIRE);
+        } else if(type == Type.FIRE){
+            this.enemyInfo.setType(Type.GRASS);
+        } else if(type == Type.GRASS){
+            this.enemyInfo.setType(Type.NORMAL);
+        } else{
+            this.enemyInfo.setType(Type.WATER);
+        }
+
+    }
+
+    public boolean checkGimmick(){
+        if(this.enemyInfo.getHealth() < 25){
+            return true;
+        } else{
+            return false;
+        }
+    }
+}
