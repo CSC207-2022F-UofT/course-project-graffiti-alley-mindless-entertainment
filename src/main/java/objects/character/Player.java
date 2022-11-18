af02a9cb9a3044a69cdeedcf90539c8b57dcfd79
Marja-Leena <marjaleena.kiik@mail.utoronto.ca>
2022-11-17 20:58:19 -0500

    Edited Player class to have currHealth and maxHealth.

--- src/main/java/objects/character/Player.java
+++ src/main/java/objects/character/Player.java
@@ -5,7 +5,9 @@ public class Player extends Character {
      * Attributes:
      * name: The name of the Player.
      * description: A description of the Player. Filled by the user at the start of the game.
-     * health: The amount of health of the Player.
+     * currHealth: The current amount of health of the Player. Decreases and increases during a battle. Never exceeds
+     *             maxHealth.
+     * maxHealth: The maximum amount of health of the Player.
      * experience: The amount of experience points of the Player. Increases after completing tasks or killing enemies.
      * level: The level of the Player. Increases after experience points reach a certain value. Unlocks various new
      *        content for the Player, such as new skills and increased health.
@@ -15,7 +17,8 @@ public class Player extends Character {
      */
     private String name;
     private String description;
-    private int health;
+    private int currHealth;
+    private int maxHealth;
     private int experience;
     private int level;
     private int type;
@@ -23,13 +26,14 @@ public class Player extends Character {
     private int money;
 
     public Player(String name, int type) {
-        /* Initializes a new Player character with name and type, an empty description, 100 health and speed, 0
-        experience and level, and 20 money.
+        /* Initializes a new Player character with name and type, an empty description, 100 currHealth and maxHealth
+        and speed, 0 experience and level, and 20 money.
          */
         super(name);
         this.description = "";
         this.type = type;
-        this.health = 100;
+        this.currHealth = 100;
+        this.maxHealth = 100;
         this.experience = 0;
         this.level = 0;
         this.speed = 100;
@@ -46,14 +50,35 @@ public class Player extends Character {
         this.description = newDesc;
     }
 
-    public int getHealth() {
-        // Return the amount of health this Player has.
-        return this.health;
+    public int getCurrHealth() {
+        // Return the amount of health this Player currently has.
+        return this.currHealth;
     }
 
-    public void changeHealth(int changeBy) {
-        // Change the Player's health by changeBy. Increases if positive, and decreases if negative.
-        this.health += changeBy;
+    public void changeCurrHealth(int changeBy) {
+        // Change the Player's current health by changeBy. Current health cannot be below zero, or higher than
+        // maxHealth. currHealth is set to 0 if it decreases below 0, and set to maxHealth if it increases more than
+        // maxHealth.
+        int newHealth = this.currHealth + changeBy;
+        if (newHealth < 0) {
+            this.currHealth = 0;
+        }
+        else if (newHealth > this.maxHealth) {
+            this.currHealth = this.maxHealth;
+        }
+        else {
+            this.currHealth = newHealth;
+        }
+    }
+
+    public int getMaxHealth() {
+        // Return the maximum amount of health this Player can have.
+        return this.maxHealth;
+    }
+
+    public void increaseMaxHealth(int changeBy) {
+        // Increase the Player's maximum health by increaseBy.
+        this.maxHealth += changeBy;
     }
 
     public int getExperience() {
