6e3f1e84f2f0a06128b448ed1b060f539e11cf2a
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-17 23:42:26 -0500

    Big change: added SmartAI and used EnemyInfo

--- src/main/java/Battle/Enemy/EnemyAI.java
+++ src/main/java/Battle/Enemy/EnemyAI.java
@@ -5,5 +5,5 @@ public interface EnemyAI {
      * @param input: input from the user in string
      * @return enemy's response to the input in string
      */
-    public String respond(String input, BattleInfo info);
+    public String respond(String input);
 }
