6adad5612ed53a653f53dc218df40f512bffbd24
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-19 14:32:55 -0500

    started test

---
+++ src/test/java/battle/enemy/EnemyInfoTest.java
@@ -0,0 +1,14 @@
+package battle.enemy;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class EnemyInfoTest {
+
+    @DisplayName("Test for get")
+    @Test
+    public void getHealthTest(){
+
+    }
+
+}
