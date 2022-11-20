8cbb0a7b9c5af4adb895f7e97fbfe05a08a5fa1a
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-11-20 14:46:50 -0500

    added new test for enemy factory

---
+++ src/test/java/battle/enemy/EnemyFactoryTest.java
@@ -0,0 +1,42 @@
+package battle.enemy;
+
+import objects.battle.Skill;
+import objects.battle.SkillType;
+import objects.battle.enemy.EnemyFactory;
+import objects.battle.enemy.EnemyInfo;
+import objects.battle.enemy.ai.DefaultAI;
+import objects.battle.enemy.ai.SmartAI;
+import objects.battle.enemy.gimmick.HealthGimmick;
+import objects.character.Boss;
+import objects.character.Enemy;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.ArrayList;
+
+public class EnemyFactoryTest {
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct enemy")
+    public void getEnemyTest(){
+        Skill skill = new Skill("fireball", 20, 10, SkillType.FIRE);
+        ArrayList<Skill> skills = new ArrayList<>();
+        skills.add(skill);
+        EnemyInfo enemyInfo = new EnemyInfo(skills, 90, 5, SkillType.FIRE);
+        Enemy enemy = new Enemy("goblin", enemyInfo, new DefaultAI(enemyInfo, 50));
+        Assert.assertEquals(EnemyFactory.getEnemy("goblin"), enemy);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct boss")
+    public void getBossTest(){
+        Skill skill = new Skill("beam", 25, 10, SkillType.EARTH);
+        ArrayList<Skill> skills = new ArrayList<>();
+        skills.add(skill);
+        EnemyInfo enemyInfo = new EnemyInfo(skills, 110, 20, SkillType.EARTH);
+        Boss boss = new Boss("goblin warrior", enemyInfo, new SmartAI(enemyInfo, 80),
+                new HealthGimmick(enemyInfo));
+        Assert.assertEquals(EnemyFactory.getEnemy("goblin warrior"), boss);
+    }
+}
