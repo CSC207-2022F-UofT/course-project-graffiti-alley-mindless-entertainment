5f42e47cf500e91034b655a602e448b6e4e60392
Charlie Tao <charlietaoxl@gmail.com>
2022-11-21 15:49:18 -0500

    Created BattleStateManager which controls the flow of the battle using the varying battle states.

---
+++ src/main/java/battlestates/BattleStateManager.java
@@ -0,0 +1,102 @@
+package battlestates;
+
+import core.StateManager;
+import game_world.objects.areas.Area;
+import interfaces.State;
+import objects.character.Player;
+
+public class BattleStateManager extends StateManager {
+    /** Class that manages the Turn states during a battle encounter. In particular, the order of
+     *  Enemy and User turns. Uses PreBattle and PostBattle to determine set-up and results of battle.
+     *  Attributes:
+     *  user: Player object representing the user
+     *  foe: Enemy object representing who the user is battling
+     */
+    private Player user;
+    private Area currArea;
+    private Enemy foe;
+
+    public BattleStateManager(Player user, Area currArea) {
+        this.user = user;
+        this.currArea = currArea;
+        this.foe = EnemyFactory.createEnemy("goblin"); // Temporary, use-case needed once decide which enemy
+    }
+
+    /**
+     * Returns the next state based on the current state. If either enemy or user are dead, ends battle and
+     * provides win or lose state accordingly.
+     * @param input string input to dictate the next state, currently unused
+     * @return the state that comes next in the battle.
+     */
+    @Override
+    public State nextState(String input) {
+        boolean userNext = user.getSpeed() >= foe.getSpeed();
+        State chosenState;
+
+        // Win condition
+        if (!foe.checkAlive()) {
+            chosenState = new WinBattleState(user, foe);
+            user.changeSpeed(100);
+        }
+        // Lose condition
+        else if (this.user.getCurrHealth() <= 0) {
+            chosenState = new LoseBattleState(user, foe);
+            user.changeSpeed(100);
+        }
+        // Normal battle turn ordering, characters can get multiple turns in a row if high enough speed.
+        else {
+            if (userNext) {
+                chosenState = new UserTurnState(user, foe);
+            }
+            else {
+                chosenState = new EnemyTurnState(user, foe, input);
+            }
+        }
+        return chosenState;
+    }
+
+    /**
+     * Calls the preinput function from the current state.
+     * If the current state is done moves onto the next state.
+     * When the current state is done and is a Win/Lose state, battle ends.
+     * TEMPORARY, instead of battle ending for losing, resets health.
+     */
+    @Override
+    public void preInput() {
+        currState.preInput();
+        boolean currStateDone = currState.isDone();
+
+        if (currStateDone) {
+            this.currState = this.nextState("");
+            if (this.currState instanceof WinBattleState) {
+                this.isDone = true;
+            }
+        }
+    }
+
+    /**
+     * Passes on the user input from the shell to the current state.
+     * If the current state is done moves onto the next state.
+     * When the current state is done and is a Win/Lose state, battle ends.
+     * TEMPORARY, instead of battle ending for losing, resets health.
+     *
+     * @param input User input given by the shell
+     */
+    @Override
+    public void postInput(String input) {
+        currState.postInput(input);
+        boolean currStateDone = currState.isDone();
+
+        if (currStateDone) {
+            this.currState = this.nextState(input);
+            if (this.currState instanceof WinBattleState) {
+                this.isDone = true;
+            }
+        }
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+}
