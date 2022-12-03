5eda2f4ef01c5f30bb2b3257e1e2db316394ed83
Juhwan Son <juhwan.sonn@gmail.com>
2022-12-03 13:58:21 -0500

    Implemented ItemFactory

---
+++ src/main/java/game_world/factories/ItemFactory.java
@@ -0,0 +1,27 @@
+package game_world.factories;
+
+import objects.character.Player;
+import objects.item.*;
+
+/**
+ * A factory class to create an item.
+ */
+public class ItemFactory {
+
+    /**
+     *
+     * @param itemType type of item
+     * @return Item object based on the itemType
+     */
+    public static Item createItem(String itemType){
+        switch (itemType) {
+            case "Sword":
+                return new Sword(Player.getLevel());
+            case "Potion":
+                return new Potion(Player.getLevel());
+            case "Armor":
+                return new Armor(Player.getLevel());
+        }
+        return null;
+    }
+}
