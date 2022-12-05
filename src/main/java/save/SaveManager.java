ed72480d18ef53fae500f3d76601512af45e9c63
Zoe <zoe.he@mail.utoronto.ca>
2022-12-05 16:21:06 -0500

    fixed SaveManager

--- src/main/java/save/SaveManager.java
+++ src/main/java/save/SaveManager.java
@@ -8,18 +8,41 @@ public class SaveManager {
     ArrayList<Save> saves;
     SaveLoader loader;
     SaveFactory creator;
+    private int MAX_SLOTS;
 
     public SaveManager() {
         loader = new SaveLoader();
         creator = new SaveFactory();
     }
 
-    public void loadFromSlot(int slot) {
-        loader.loadSave(saves.get(slot), entities);
+    public int getMaxSlots() {
+        return MAX_SLOTS;
     }
 
-    public void saveAtSlot() {
-        saves.add(creator.createSave(entities));
+    /**
+     * @param slot the slot to load file from
+     * @return whether loaded successfully or not
+     */
+    public boolean loadFromSlot(int slot) {
+        Save s = saves.get(slot);
+        if (s == null) {
+            return false;
+        }
+        loader.loadSave(s, entities);
+        return true;
+    }
+
+    /**
+     * @param slot the slot to save file at
+     * @return whether saved successfully or not
+     */
+    public boolean saveAtSlot(int slot) {
+        if (saves.get(slot) == null) {
+            Save s = creator.createSave(entities);
+            saves.set(slot, s);
+            return true;
+        }
+        return false;
     }
 }
 
