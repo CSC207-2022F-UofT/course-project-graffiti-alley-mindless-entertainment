feb57e30477185c57426c8b31b58d3044656ef69
bw55555 <brianwang2013@gmail.com>
2022-12-07 12:09:30 -0500

    inject eventFactory dependency instead of itemPickupEventFactory

--- src/test/java/worldgen/EventManagerTest.java
+++ src/test/java/worldgen/EventManagerTest.java
@@ -1,10 +1,10 @@
 package worldgen;
 
+import game_world.factories.EventFactory;
 import game_world.factories.ItemPickUpEventFactory;
 import game_world.managers.AreaManager;
 import game_world.managers.EventManager;
 import game_world.objects.Location;
-import game_world.objects.events.ItemPickUpEvent;
 import objects.inventory.Inventory;
 import org.junit.jupiter.api.Test;
 
@@ -13,8 +13,9 @@ public class EventManagerTest {
     @Test
     void testEventManagerMethods() {
         Inventory inventory = new Inventory();
-        ItemPickUpEventFactory i = new ItemPickUpEventFactory(inventory);
-        EventManager eventManager = new EventManager(i);
+        ItemPickUpEventFactory itemPickUpEventFactory = new ItemPickUpEventFactory(inventory);
+        EventFactory eventFactory = new EventFactory(itemPickUpEventFactory);
+        EventManager eventManager = new EventManager(eventFactory);
         AreaManager areaManager = new AreaManager(eventManager, new Location());
     }
 
