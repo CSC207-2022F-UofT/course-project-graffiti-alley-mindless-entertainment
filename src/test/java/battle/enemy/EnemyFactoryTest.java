d93804eb15b9b49e64b0652dd149d8bf3957c996
Yasutakahi <yasutaka.hisano@mail.utoronto.ca>
2022-12-07 15:27:58 -0500

    adding the tests for test coverage and also added enemies, bosses, and skills in the database

--- src/test/java/battle/enemy/EnemyFactoryTest.java
+++ src/test/java/battle/enemy/EnemyFactoryTest.java
@@ -24,7 +24,7 @@ public class EnemyFactoryTest {
 
     @Test
     @DisplayName("This test checks if the EnemyFactory class returns correct enemy")
-    public void getEnemyTest() throws Exception {
+    public void getEnemyTest(){
         Skill skill = new Skill("fireball", 20, 10, SkillType.FIRE);
         ArrayList<Skill> skills = new ArrayList<>();
         skills.add(skill);
@@ -42,6 +42,60 @@ public class EnemyFactoryTest {
         Assertions.assertEquals(enemy.getType(), SkillType.FIRE);
         Assertions.assertEquals(enemy.getHealth(), 100);
         Assertions.assertEquals(enemy.getName(), "goblin");
+        Assertions.assertTrue(enemy.getEnemyAI() instanceof DefaultAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct enemy")
+    public void getEnemySlimeTest() {
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter enemy = enemyFactory.createEnemy("slime");
+        Assertions.assertEquals(enemy.getSkill(0).getDamage(), 20);
+        Assertions.assertEquals(enemy.getSkill(0).getType(), SkillType.WATER);
+        Assertions.assertEquals(enemy.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(enemy.getSkill(0).getName(),
+                "waterfall");
+        Assertions.assertEquals(enemy.getReputation(), 5);
+        Assertions.assertEquals(enemy.getSpeed(), 90);
+        Assertions.assertEquals(enemy.getType(), SkillType.WATER);
+        Assertions.assertEquals(enemy.getHealth(), 100);
+        Assertions.assertEquals(enemy.getName(), "slime");
+        Assertions.assertTrue(enemy.getEnemyAI() instanceof DefaultAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct enemy")
+    public void getEnemySkeletonTest() {
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter enemy = enemyFactory.createEnemy("skeleton");
+        Assertions.assertEquals(enemy.getSkill(0).getDamage(), 20);
+        Assertions.assertEquals(enemy.getSkill(0).getType(), SkillType.AIR);
+        Assertions.assertEquals(enemy.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(enemy.getSkill(0).getName(),
+                "air cutter");
+        Assertions.assertEquals(enemy.getReputation(), 5);
+        Assertions.assertEquals(enemy.getSpeed(), 90);
+        Assertions.assertEquals(enemy.getType(), SkillType.AIR);
+        Assertions.assertEquals(enemy.getHealth(), 100);
+        Assertions.assertEquals(enemy.getName(), "skeleton");
+        Assertions.assertTrue(enemy.getEnemyAI() instanceof DefaultAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct enemy")
+    public void getEnemyGrassHopperTest() {
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter enemy = enemyFactory.createEnemy("grass hopper");
+        Assertions.assertEquals(enemy.getSkill(0).getDamage(), 20);
+        Assertions.assertEquals(enemy.getSkill(0).getType(), SkillType.EARTH);
+        Assertions.assertEquals(enemy.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(enemy.getSkill(0).getName(),
+                "waterfall");
+        Assertions.assertEquals(enemy.getReputation(), 5);
+        Assertions.assertEquals(enemy.getSpeed(), 90);
+        Assertions.assertEquals(enemy.getType(), SkillType.EARTH);
+        Assertions.assertEquals(enemy.getHealth(), 100);
+        Assertions.assertEquals(enemy.getName(), "grass hopper");
         Assertions.assertTrue(enemy.getEnemyAI() instanceof DefaultAI);
     }
 
@@ -67,6 +121,63 @@ public class EnemyFactoryTest {
         Assertions.assertEquals(boss.getType(), SkillType.EARTH);
         Assertions.assertEquals(boss.getHealth(), 100);
         Assertions.assertEquals(boss.getName(), "goblin warrior");
+        Assertions.assertTrue(boss.getEnemyAI() instanceof
+                SmartAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct boss")
+    public void getBossKingSlimeTest(){
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter boss = enemyFactory.createEnemy("slime king");
+        Assertions.assertEquals(boss.getSkill(0).getDamage(), 25);
+        Assertions.assertEquals(boss.getSkill(0).getType(), SkillType.WATER);
+        Assertions.assertEquals(boss.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(boss.getSkill(0).getName(),
+                "water punch");
+        Assertions.assertEquals(boss.getReputation(), 20);
+        Assertions.assertEquals(boss.getSpeed(), 110);
+        Assertions.assertEquals(boss.getType(), SkillType.WATER);
+        Assertions.assertEquals(boss.getHealth(), 100);
+        Assertions.assertEquals(boss.getName(), "slime king");
+        Assertions.assertTrue(boss.getEnemyAI() instanceof
+                SmartAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct boss")
+    public void getBossSkeletonKingTest(){
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter boss = enemyFactory.createEnemy("skeleton king");
+        Assertions.assertEquals(boss.getSkill(0).getDamage(), 25);
+        Assertions.assertEquals(boss.getSkill(0).getType(), SkillType.AIR);
+        Assertions.assertEquals(boss.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(boss.getSkill(0).getName(),
+                "air punch");
+        Assertions.assertEquals(boss.getReputation(), 20);
+        Assertions.assertEquals(boss.getSpeed(), 110);
+        Assertions.assertEquals(boss.getType(), SkillType.AIR);
+        Assertions.assertEquals(boss.getHealth(), 100);
+        Assertions.assertEquals(boss.getName(), "skeleton king");
+        Assertions.assertTrue(boss.getEnemyAI() instanceof
+                SmartAI);
+    }
+
+    @Test
+    @DisplayName("This test checks if the EnemyFactory class returns correct boss")
+    public void getBossKingWizardTest(){
+        EnemyFactory enemyFactory = new EnemyFactory();
+        EnemyFighter boss = enemyFactory.createEnemy("king wizard");
+        Assertions.assertEquals(boss.getSkill(0).getDamage(), 25);
+        Assertions.assertEquals(boss.getSkill(0).getType(), SkillType.FIRE);
+        Assertions.assertEquals(boss.getSkill(0).getLag(), 10);
+        Assertions.assertEquals(boss.getSkill(0).getName(),
+                "fire punch");
+        Assertions.assertEquals(boss.getReputation(), 20);
+        Assertions.assertEquals(boss.getSpeed(), 110);
+        Assertions.assertEquals(boss.getType(), SkillType.FIRE);
+        Assertions.assertEquals(boss.getHealth(), 100);
+        Assertions.assertEquals(boss.getName(), "king wizard");
         Assertions.assertTrue(boss.getEnemyAI() instanceof
                 SmartAI);
     }
