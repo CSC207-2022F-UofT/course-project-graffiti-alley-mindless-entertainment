78ee604a8385859eb7317865087802a8f3cf6280
Charlie Tao <charlietaoxl@gmail.com>
2022-12-08 10:14:03 -0500

    Created BattleStateFactory factory class

---
+++ src/main/java/battlestates/factories/BattleStateFactory.java
@@ -0,0 +1,30 @@
+package battlestates.factories;
+
+import battlestates.states.*;
+import interfaces.State;
+import objects.battle.BattleEntityInteractor;
+
+public class BattleStateFactory {
+    /**
+     * Factory to create Battle States to avoid dependencies
+     * Attributes:
+     * battleEntityInteractor: Interactor that deals with checking stats of user and foe in battle
+     */
+    public BattleEntityInteractor battleEntityInteractor;
+
+    public BattleStateFactory(BattleEntityInteractor battleEntityInteractor) {
+        this.battleEntityInteractor = battleEntityInteractor;
+    }
+    public State createWinBattleState() {
+        return new WinBattleState(battleEntityInteractor.getUser(), battleEntityInteractor.getFoe());
+    }
+    public State createLoseBattleState() {
+        return new LoseBattleState(battleEntityInteractor.getUser(), battleEntityInteractor.getFoe());
+    }
+    public State createEnemyTurnState(String input) {
+        return new EnemyTurnState(battleEntityInteractor.getUser(), battleEntityInteractor.getFoe(), input);
+    }
+    public State createUserTurnState() {
+        return new UserTurnState(battleEntityInteractor.getUser(), battleEntityInteractor.getFoe());
+    }
+}
